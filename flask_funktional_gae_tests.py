d4651d981a037c732575170f3db9c5bbe295410f
Gregory Nicholas <nicholas.g.gregory@gmail.com>
2013-07-06 20:28:37 -0500

    cleaning up docs, docstrings, readme for packaging.

---
+++ flask_funktional_gae_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+  flask_funktional_gae_tests
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  :copyright: (c) 2013 by gregorynicholas.
+  :license: MIT, see LICENSE for more details.
+"""
+import unittest
+
+
+if __name__ == "__main__":
+  unittest.main()
